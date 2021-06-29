e3db09390215a46b1f9e736e589da1f7dce90d41
Thomas Le Duc <dev@tleduc.de>
2021-06-29 09:13:51 +0200

    Add test for simple renaming of file

--- app/tests/integration_test.rs
+++ app/tests/integration_test.rs
@@ -1,23 +1,41 @@
 use serial_test::serial;
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::{self, File},
+    io,
+    path::Path,
+};
 
-use rn;
+use rn::{self, Config};
 
 #[test]
 #[serial]
 fn rename_file() -> Result<(), Box<dyn Error>> {
-    // todo
-    let executable_name = "/bin/rn";
-    let old_name = "foo_file";
+    setup()?;
+    let source = format!("{}/{}", base_dir(), "foo_file");
     let new_name = "bar_file";
-    let args = vec![
-        executable_name.to_owned(),
-        old_name.to_owned(),
-        new_name.to_owned(),
-    ];
-    let config = rn::Config::new(&args)?;
+    File::create(&source)?;
 
+    let config = create_config(&source, &new_name)?;
     rn::run(config)?;
 
+    let target = format!("{}/{}", base_dir(), new_name);
+    assert!(Path::new(&target).exists());
     Ok(())
+}
+
+fn create_config(source: &str, new_name: &str) -> Result<Config, &'static str> {
+    let args = vec!["/bin/rn".to_owned(), source.to_owned(), new_name.to_owned()];
+    rn::Config::new(&args)
+}
+
+fn setup() -> Result<(), io::Error> {
+    if Path::new(&base_dir()).exists() {
+        fs::remove_dir_all(base_dir())?;
+    }
+    fs::create_dir_all(&base_dir())
+}
+
+fn base_dir() -> String {
+    String::from("tests/data/rename")
 }
