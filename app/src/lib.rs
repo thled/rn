5d3a794fd3f481b4fcad4f67cb959c1ec4313ea3
Thomas Le Duc <dev@tleduc.de>
2021-06-29 20:52:54 +0200

    Create new file with dynamic path

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,10 +1,11 @@
-use std::{error::Error, fs::File};
+use std::{error::Error, fs::File, path::Path};
 
 pub use config::Config;
 
 mod config;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    File::create(format!("tests/data/rename/{}", config.new_name))?;
+    let path = Path::new(&config.old_name).parent().unwrap().to_string_lossy();
+    File::create(format!("{}/{}", path,config.new_name))?;
     Ok(())
 }
