bc98829f0a018a383926b4ee714872123f01e5ce
Thomas Le Duc <dev@tleduc.de>
2021-06-30 10:31:33 +0200

    Refactor tests to change dir in setup

--- app/tests/integration_test.rs
+++ app/tests/integration_test.rs
@@ -1,4 +1,5 @@
 use serial_test::serial;
+use std::env;
 use std::io::Write;
 use std::{
     error::Error,
@@ -92,12 +93,37 @@ fn remove_old_file() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[test]
+#[serial]
+fn no_path() -> Result<(), Box<dyn Error>> {
+    setup()?;
+    let source = format!("{}/{}", base_dir(), "foo_file");
+    File::create(&source)?;
+    let new_name = "bar_file";
+
+    let config = create_config(&source, &new_name)?;
+    rn::run(config)?;
+
+    let target = format!("{}/{}", base_dir(), new_name);
+    assert!(Path::new(&target).exists());
+    Ok(())
+}
+
 fn create_config(source: &str, new_name: &str) -> Result<Config, &'static str> {
     let args = vec!["/bin/rn".to_owned(), source.to_owned(), new_name.to_owned()];
     rn::Config::new(&args)
 }
 
+fn change_dir_to_tests_data() -> Result<(), io::Error> {
+    let cwd = env::current_dir().unwrap();
+    if !cwd.ends_with("tests/data") {
+        env::set_current_dir(format!("{}/tests/data", cwd.to_string_lossy()))?;
+    }
+    Ok(())
+}
+
 fn setup() -> Result<(), io::Error> {
+    change_dir_to_tests_data()?;
     if Path::new(&base_dir()).exists() {
         fs::remove_dir_all(base_dir())?;
     }
@@ -105,5 +131,5 @@ fn setup() -> Result<(), io::Error> {
 }
 
 fn base_dir() -> String {
-    String::from("tests/data/rename")
+    String::from("rename")
 }
