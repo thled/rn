311e23a118f5d61e7b5ac6ed4ed61b2a378bfa1e
Thomas Le Duc <dev@tleduc.de>
2021-07-01 22:56:10 +0200

    Add test for dir with path

--- app/tests/integration_test.rs
+++ app/tests/integration_test.rs
@@ -136,6 +136,23 @@ fn remove_old_dir() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[test]
+#[serial]
+fn dir_with_path() -> Result<(), Box<dyn Error>> {
+    setup()?;
+    let dir = "dir";
+    let source = format!("{}/{}", dir, "old_dir_name");
+    fs::create_dir_all(&source)?;
+    let new_name = "new_dir_name";
+
+    let config = create_config(&source, new_name)?;
+    rn::run(config)?;
+
+    let target = format!("{}/{}", dir, new_name);
+    assert!(Path::new(&target).is_dir());
+    Ok(())
+}
+
 fn create_config(source: &str, new_name: &str) -> Result<Config, &'static str> {
     let args = vec!["/bin/rn".to_owned(), source.to_owned(), new_name.to_owned()];
     rn::Config::new(&args)
