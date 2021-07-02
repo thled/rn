7ac1af1d182c86f05e19e992ddab21dbb237238f
Thomas Le Duc <dev@tleduc.de>
2021-07-02 09:08:29 +0200

    Refactor by using `fs::rename()`

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
     error::Error,
-    fs, io,
+    fs,
     path::{Path, PathBuf},
 };
 
@@ -10,45 +10,26 @@ mod config;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let source = Path::new(&config.source);
-    if source.is_file() {
-        create_new_file(&source, &config.new_name)?;
-        remove_old_file(&source)?;
-    } else {
-        create_new_dir(&source, &config.new_name)?;
-        // remove_old_dir(&source)?;
-    }
+    rename(&source, &config.new_name)?;
     Ok(())
 }
 
-fn create_new_dir(source: &Path, new_name: &str) -> Result<(), Box<dyn Error>> {
-    let mut target = PathBuf::new();
-    if has_path(source) {
-        let path = source.parent().unwrap();
-        target.push(path);
-    }
-    target.push(new_name);
-
+fn rename(source: &Path, new_name: &str) -> Result<(), Box<dyn Error>> {
+    let target = build_target(source, new_name);
     fs::rename(&source, &target)?;
     Ok(())
 }
 
-fn create_new_file(source: &Path, new_name: &str) -> Result<(), Box<dyn Error>> {
+fn build_target(source: &Path, new_name: &str) -> PathBuf {
     let mut target = PathBuf::new();
     if has_path(source) {
         let path = source.parent().unwrap();
         target.push(path);
     }
     target.push(new_name);
-
-    let content = fs::read(source)?;
-    fs::write(&target, &content)?;
-    Ok(())
+    target
 }
 
 fn has_path(source: &Path) -> bool {
     source.components().count() > 1
-}
-
-fn remove_old_file(source: &Path) -> io::Result<()> {
-    fs::remove_file(source)
 }
